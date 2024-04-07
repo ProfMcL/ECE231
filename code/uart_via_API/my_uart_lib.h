5e2d07f95e966a6821db5994168b93f576bfa377
David McLaughlin <156027883+ProfMcL@users.noreply.github.com>
2024-04-07 17:46:30 -0400

    Update my_uart_lib.h

--- code/uart_via_API/my_uart_lib.h
+++ code/uart_via_API/my_uart_lib.h
@@ -6,34 +6,22 @@
 #include <avr/io.h>
 #include <string.h>
 
-/*
-* Initialize the UART
-*
-* Enables the UART transmitter; Sets 8 bit character size
+/* Initialize the UART: Enables the UART transmitter; Sets 8 bit character size
 * Sets baud rate to 9600 for 16 MHz crystal
-*
 * Arguments: none
-* Returns: none
-*/
+* Returns: none */
 void uart_init(void);
 
-/*
-* Transmit a single character via UART
-*
+/* Transmit a single character via UART
 * Arguments: 
 *       letter - ASCII character to be transmitted
-* Returns: none
-*/
+* Returns: none */
 void uart_send(unsigned char letter);
 
-/*
-* Transmit a character string via UART
-*
+/* Transmit a character string via UART.
 * Sends the string, char by char, to the UART
 * via uart_send()
-*
 * Arguments: 
 *       *stringAddress - pointer to the string
-* Returns: none
-*/
+* Returns: none */
 void send_string(char *stringAddress);
