49d27b73f328f7b028a599668c583bc8095df0f5
David McLaughlin <156027883+ProfMcL@users.noreply.github.com>
2024-04-24 10:24:50 -0400

    Update adc_serial_volts.c

--- assignments/Lab 8.2/lab8.2 libraries/adc_serial_volts.c
+++ assignments/Lab 8.2/lab8.2 libraries/adc_serial_volts.c
@@ -2,7 +2,9 @@
 * adc_serial_volts.c  Reads analog voltage and writes
 * both digital value and corresponding voltage result 
 * to the serial port and the SSD1306 OLED
-* Version 1.0 D. McLaughlin initial release
+* Version     Author            Date     Description
+*  1.0        D. McLaughlin     4/22/24  initial release
+*  1.1        D. McLaughlin     4/25/24  better formatting and comments
 ***********************************************/
 
 #include "my_adc_lib.h"
@@ -17,25 +19,29 @@
 int main(void) {
     unsigned int digitalValue;
     double analogValue;
-    char buffer[6];
+    char buffer1[6], buffer2[6];
     
     uart_init();
     adc_init();
     OLED_Init();
 
     while (1) {
-        digitalValue = get_adc();
-        itoa(digitalValue, buffer, 10); //convert to a decimal character string
+
+        // Take an ADC reading and create ASCII strings for digital representation & corresponding analog voltage
+        digitalValue = get_adc();                     // The an ADC reading
+        analogValue = digitalValue * V_REF / 1023;    // Convert digitalValue to corresponding analog voltage
+        itoa(digitalValue, buffer1, 10);              // Convert the int to a character string in decimal
+        dtostrf(analogValue, 5, 3, buffer2);          // Convert floating point to a character string
+        
+        // Send the digital and analog strings to the serial monitor
         send_string("Digital: ");
-        send_string(buffer);
-        analogValue = digitalValue * V_REF / 1023;
-        dtostrf(analogValue, 5, 3, buffer); // Convert to a character string
+        send_string(buffer1);
         send_string("   Analog: ");
-        send_string(buffer);
+        send_string(buffer2);
         uart_send(13);
         uart_send(10);
 
-        // Send the digital and analog values to the OLED
+        // Display the digital value and the analog character string on the OLED
         OLED_GoToLine(1);
         OLED_DisplayString("Digital: ");
         OLED_GoToLine(3);
@@ -43,7 +49,7 @@ int main(void) {
         OLED_GoToLine(5);
         OLED_DisplayString("Analog:");
         OLED_GoToLine(6);
-        OLED_DisplayString(buffer);
+        OLED_DisplayString(buffer2);
         _delay_ms(1000);
     }
   
